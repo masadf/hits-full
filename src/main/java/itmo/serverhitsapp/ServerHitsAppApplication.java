51d20aab87cde5f1d231291b421779e3d1e91a4d
Marin Savva <geg_gge_geg@mail.ru>
2022-12-07 20:50:51 +0300

    init

---
+++ src/main/java/itmo/serverhitsapp/ServerHitsAppApplication.java
@@ -0,0 +1,13 @@
+package itmo.serverhitsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerHitsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerHitsAppApplication.class, args);
+    }
+
+}
